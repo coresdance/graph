c9c53b4148bb8a0f48fa7094258318eeb6b120e4
coresdance <coresdance@gmail.com>
2019-11-25 17:48:03 +0800

    add test part in main.cpp

--- main.cpp
+++ main.cpp
@@ -19,6 +19,14 @@ int main() {
             );
         }
 
-    
+    for (auto it : g)
+        if (typeid(it) == typeid(git_test::circle)) {
+            it->prtGraph();
+        } else if (typeid(it) == typeid(git_test::square)) {
+            it->prtGraph();
+        } else {
+            //异常
+            std::cout << "异常" << std::endl;
+        }
     return 0;
 }
